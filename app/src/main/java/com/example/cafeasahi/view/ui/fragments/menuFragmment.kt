389f135fc02d9f7716273bf11346f1771b94360d
Sally Cerquera <sallyc710@gmail.com>
2022-11-05 10:34:31 -0500

    actualizaciones

---
+++ app/src/main/java/com/example/cafeasahi/view/ui/fragments/menuFragmment.kt
@@ -0,0 +1,33 @@
+package com.example.cafeasahi.view.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
+import com.example.cafeasahi.R
+
+
+class fragment_menu : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val cardLib=view.findViewById<ImageView>(R.id.carcafes)
+        cardLib.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment_menu_to_cafesFragment)
+        }
+    }
+
+
+}
