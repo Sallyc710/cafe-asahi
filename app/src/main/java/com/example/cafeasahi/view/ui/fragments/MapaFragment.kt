4991dff935e63cb946e8f7f93ed30215ecb0d3a4
malyjpl <malyjpl@gmail.com>
2022-11-18 14:41:57 -0500

    agregando codigo de Sally con mapa

--- app/src/main/java/com/example/cafeasahi/view/ui/fragments/MapaFragment.kt
+++ app/src/main/java/com/example/cafeasahi/view/ui/fragments/MapaFragment.kt
@@ -1,20 +1,100 @@
 package com.example.cafeasahi.view.ui.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.cafeasahi.R
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MapaFragment : Fragment() {
+class MapaFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa, container, false)
+        val view = inflater.inflate(R.layout.fragment_mapa, container, false)
+        val mapFragment =
+            this.childFragmentManager.findFragmentById(R.id.map_view) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        return view
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        val colombia = com.google.android.gms.maps.model.LatLng(4.570868, -74.297333)
+        map?.let {
+            this.googleMap = it
+            map.addMarker(MarkerOptions().position(colombia))
+        }
+        enableLocation()
+
+    }
+
+    private fun islocationPermissionGrated() = ContextCompat.checkSelfPermission(
+        this.requireContext(),
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocation() {
+        if (!::googleMap.isInitialized) return
+        if (islocationPermissionGrated()) {
+            googleMap.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
+
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this.requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            Toast.makeText(context, "requiere activar permisos en ajustes", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this.requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION
+            )
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                googleMap.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(
+                    context,
+                    "Para activar la localización ve a ajustes y acepta los permisos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else -> {}
+        }
     }
 }
