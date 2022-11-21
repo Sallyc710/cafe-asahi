8c6d28b1e8da32fc3e5d5a14b1a670ee580c1b37
Ronnal <reortizc@unal.edu.co>
2022-11-21 08:37:55 -0500

    Navegacion productos funcional

--- app/src/main/java/com/example/cafeasahi/view/ui/fragments/ProducFragment.kt
+++ app/src/main/java/com/example/cafeasahi/view/ui/fragments/ProducFragment.kt
@@ -1,10 +1,8 @@
 package com.example.cafeasahi.view.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,7 +16,9 @@ import com.example.cafeasahi.model.cafes
 import com.example.cafeasahi.view.adapter.OnBookItemClickListener
 import com.example.cafeasahi.viewmodel.CafeViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 
 class ProducFragment : Fragment(), OnBookItemClickListener {
@@ -35,6 +35,7 @@ class ProducFragment : Fragment(), OnBookItemClickListener {
         savedInstanceState: Bundle?
     ): View? {
         val view= inflater.inflate(R.layout.fragment_produc, container, false)
+        firebaseAuth=Firebase.auth
         recyclerLib=view.findViewById(R.id.recyclerview)
         adapter=ProducAdapter(requireContext(), this)
         recyclerLib.layoutManager=LinearLayoutManager(context)
@@ -60,6 +61,49 @@ class ProducFragment : Fragment(), OnBookItemClickListener {
         })
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_navigation_toolbar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem):Boolean {
+        return when(item.itemId){
+            R.id.home->{
+                findNavController().navigate(R.id.action_producFragment_to_homeFragment)
+                true
+            }
+            R.id.perfil->{
+                findNavController().navigate(R.id.action_producFragment_to_perfilFragment)
+                true
+            }
+            R.id.Map->{
+                findNavController().navigate(R.id.action_producFragment_to_mapaFragment)
+                true
+            }
+
+            R.id.compras->{
+                findNavController().navigate(R.id.action_producFragment_to_compraFragment)
+                true
+            }
+            R.id.ayuda->{
+                findNavController().navigate(R.id.action_producFragment_to_ayudaFragment)
+                true
+            }
+
+            R.id.cerrar->{
+                firebaseAuth.signOut()
+                findNavController().navigate(R.id.action_producFragment_to_loginActivity)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -67,10 +111,9 @@ class ProducFragment : Fragment(), OnBookItemClickListener {
         btn.setOnNavigationItemReselectedListener {
             when(it.itemId){
                 R.id.home -> findNavController().navigate(R.id.action_producFragment_to_homeFragment)
-                R.id.perfil -> findNavController().navigate(R.id.action_producFragment_to_perfilFragment)
-                R.id.mapa -> findNavController().navigate(R.id.action_producFragment_to_mapaFragment)
                 R.id.carrito -> findNavController().navigate(R.id.action_producFragment_to_compraFragment)
-                R.id.favorit -> findNavController().navigate(R.id.action_producFragment_to_favoritosFragment)
+                R.id.help2 -> findNavController().navigate(R.id.action_producFragment_to_ayudaFragment)
+                R.id.mapa -> findNavController().navigate(R.id.action_producFragment_to_mapaFragment)
             }
         }
     }
