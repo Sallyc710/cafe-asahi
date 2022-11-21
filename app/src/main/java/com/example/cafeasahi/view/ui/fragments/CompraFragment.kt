f266ceccc886eb3bff16185a1125b70b57df21ec
Ronnal <reortizc@unal.edu.co>
2022-11-21 08:56:56 -0500

    Navegacion funcional completa

--- app/src/main/java/com/example/cafeasahi/view/ui/fragments/CompraFragment.kt
+++ app/src/main/java/com/example/cafeasahi/view/ui/fragments/CompraFragment.kt
@@ -1,23 +1,70 @@
 package com.example.cafeasahi.view.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.cafeasahi.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class CompraFragment : Fragment()  {
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compra, container, false)
+        val view=inflater.inflate(R.layout.fragment_compra, container, false)
+        firebaseAuth=Firebase.auth
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_navigation_toolbar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem):Boolean {
+        return when(item.itemId){
+            R.id.home->{
+                findNavController().navigate(R.id.action_compraFragment_to_homeFragment)
+                true
+            }
+            R.id.perfil->{
+                findNavController().navigate(R.id.action_compraFragment_to_perfilFragment)
+                true
+            }
+            R.id.Map->{
+                findNavController().navigate(R.id.action_compraFragment_to_mapaFragment)
+                true
+            }
+
+            R.id.ayuda->{
+                findNavController().navigate(R.id.action_compraFragment_to_ayudaFragment)
+                true
+            }
+            R.id.cafes->{
+                findNavController().navigate(R.id.action_compraFragment_to_producFragment)
+                true
+            }
+
+            R.id.cerrar->{
+                firebaseAuth.signOut()
+                findNavController().navigate(R.id.action_compraFragment_to_loginActivity)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
 
@@ -28,10 +75,9 @@ class CompraFragment : Fragment()  {
         btn.setOnNavigationItemReselectedListener {
             when(it.itemId){
                 R.id.home -> findNavController().navigate(R.id.action_compraFragment_to_homeFragment)
-
-
-                R.id.product -> findNavController().navigate(R.id.action_compraFragment_to_producFragment)
                 R.id.help2 -> findNavController().navigate(R.id.action_compraFragment_to_ayudaFragment)
+                R.id.product -> findNavController().navigate(R.id.action_compraFragment_to_producFragment)
+                R.id.mapa -> findNavController().navigate(R.id.action_compraFragment_to_mapaFragment)
             }
         }
     }
